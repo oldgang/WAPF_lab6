e0e14d65229df39c17c39e829c1536a20de43100
oldgang <iceman16999@gmail.com>
2023-01-13 18:16:57 +0100

    init

---
+++ bookmarks.js
@@ -0,0 +1,6 @@
+const bookmarks = [
+    { name: "Gmail", url: "https://inbox.google.com" },
+    { name: "GitHub", url: "https://github.com" },
+    { name: "Drive", url: "https://drive.google.com" },
+    { name: "Inbox", url: "https://webmail.pg.edu.pl/appsuite/"},
+];
